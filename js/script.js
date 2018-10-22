481d00439fb45adcd7d4bc49e7e75b0c96e76220
Slawomir Masluk <slawomirmasluk@gmail.com>
2018-10-22 12:18:55 +0200

    Fix global variables issue in script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 'use strict'
 
+
+Start();
+
+function Start () {
+
     var prefix = "https://cors-anywhere.herokuapp.com/";
     var tweetLink = "https://twitter.com/intent/tweet?text=";
     var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
@@ -37,3 +42,4 @@
         })
     });
 
+}
